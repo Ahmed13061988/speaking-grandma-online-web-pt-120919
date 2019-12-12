0493b3822baadec240b669eb0c05dc6cb71efa96
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-12 21:27:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,9 @@
-def speak_with_grandma ("how are you")
-if "how are you ".downcase
+def speak_with_grandma (phrase="I LOVE YOU GRANDMA")
+if phrase.downcase
   puts "HUH?!SPEAK UP SONNY!"
-elsif puts 
-
-
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+elsif phrase.upcase
+puts "NO,NOT SINCE 1938!"
+else phrase =="I LOVE GRANDMA"
+return "I LOVE YOU TOO PUMPKIN"
+end 
+end 
