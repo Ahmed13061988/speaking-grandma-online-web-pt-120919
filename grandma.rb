4d055c7697e9a7123af6d4d504d42a7a4d667bf1
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-12 21:31:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_with_grandma(phrase="I LOVE YOU GRANDMA")
+def speak_with_grandma(phrase ="I LOVE YOU GRANDMA")
 if phrase.downcase
   return "HUH?!SPEAK UP SONNY!"
 elsif phrase.upcase
 return "NO,NOT SINCE 1938!"
-else phrase =="I LOVE GRANDMA"
+else phrase == "I LOVE YOU GRANDMA"
 return "I LOVE YOU TOO PUMPKIN"
 end 
 end 
