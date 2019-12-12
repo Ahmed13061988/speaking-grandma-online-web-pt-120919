b7e28ee8f79f00059201fbe847e126eadc6bff74
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-12 20:56:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
-# Write a speak_to_grandma method.
+def speak_with_grandma ("how are you")
+if 
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
